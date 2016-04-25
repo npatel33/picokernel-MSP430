8b27757697a6ca41a57be749352f21c508517b93
npatel33 <npatel33@gmu.edu>
2016-04-25 16:45:23 -0400

    Changed to work with TI-GCC

--- kernel.c
+++ kernel.c
@@ -1,7 +1,7 @@
-#include<msp430g2553.h>
-#include<stdint.h>
+#include<msp430.h>
+#include<in430.h>
+//#include<stdint.h>
 #include "kernel.h"
-#include <legacymsp430.h>
 
 //task control block(tcb) base address for task id 0.
 #define TCB_BASE 0x03c0
@@ -10,9 +10,9 @@
 #define TASK_SIZE 64
 
 //save context  
-#define SAVE_CONTEXT()    asm("pop r14 \n"\
-			      "pop r15 \n"\
-			      "push r4 \n"\
+#define SAVE_CONTEXT()    asm("push r0 \n"\
+				  "push r2 \n"\
+				  "push r4 \n"\
 			      "push r5 \n"\
 			      "push r6 \n"\
 			      "push r7 \n"\
@@ -38,9 +38,10 @@
 			      "pop r6 \n"\
 			      "pop r5 \n"\
 			      "pop r4 \n"\
-			      "reti \n")
+			      "pop r2 \n"\
+				  "pop r0 \n")
 			   
-//#ifdef DefineMaxTask(MAX_TASK,Val) {uint8_t MAX_TASK;MAX_TASK=Val;}
+
 #ifndef MAX_TASK
 #define MAX_TASK 2
 #endif
@@ -48,38 +49,42 @@
 uint8_t CurrentTask=0; //track current running task
 uint16_t Counter=0;
 
-struct sTask{//Task Structure
-
-	uint16_t Id;		//Task id
-	uint16_t Address;	//Task Address
-	uint16_t PrevStack;	//Task previous SP position
-	uint16_t TCBLoc;	//TCB location
-
-}Task[MAX_TASK];
+struct sTask Task[MAX_TASK];
 
 
 void SchedulerInit(void){
 	
-	dint();			//disable global interrupt
+
+	__disable_interrupt();			//disable global interrupt
+	__nop();
 
 	TACTL|=TACLR + MC_0;	//clear/reset and stop the timer A
-	TACTL |= TASSEL_2 ;//Setting SMCLK as clock source
+	TACTL |= TASSEL_2 + ID_2;//Setting SMCLK as clock source
 	TACCTL0 |= CCIE;	//enable compare interrupt
 	TACCTL0 &= ~(CCIFG);	//clear compare interrupt flag
 	TAR=0;			//Timer value set to 0
-	TACCR0=100;		//Compare register value for 1 ms period
+	TACCR0=60;		//Compare register value for 10 ms period
+	//TACCR0=3000; //500ms
 	//eint();			//Enable global interrupt
+
 	TACTL |= MC_1;		//Start timer A in up mode
+
 
 }
 
+
+void SchedulerISR(void) __attribute__((interrupt (TIMER0_A0_VECTOR),naked));
 /* scheduler with round robin algorithm */
-interrupt (TIMER0_A0_VECTOR) SchedulerISR(void){    //perform context switching
+void SchedulerISR(void){    //perform context switching
 	
-	P1OUT |= (1<<7);
+
+	//P1OUT ^= (1<<0);
+
+	//P1OUT |= (1<<7);
 	SAVE_CONTEXT(); //save current task context
 	
-	asm("mov r1,%[dest]":[dest]"=r"(Task[CurrentTask].PrevStack)::"r1");
+
+	asm("mov R1,%[dest]":[dest]"=r"(Task[CurrentTask].prevStack)::"R1");
 	
 	CurrentTask++; //point to next task
 	if(CurrentTask>=MAX_TASK){
@@ -87,48 +92,79 @@ interrupt (TIMER0_A0_VECTOR) SchedulerISR(void){    //perform context switching
 	}
 
 	//load next task's stack pointer
-	asm("mov %[src],r1"::[src]"r"(Task[CurrentTask].PrevStack):"r1");
+	asm("mov %[src],R1"::[src]"r"(Task[CurrentTask].prevStack):"R1");
 	
-	P1OUT &= ~(1<<7);
+	//P1OUT &= ~(1<<7);
 	RESTORE_CONTEXT();//restore next task's context
 
+}
 
-}	
-
-void TaskInit(void (*pFun)(void),uint8_t TaskId){  //initialize tasks and their stack frame
+void TaskInit(void (*pFun)(void),uint16_t taskId){  //initialize tasks and their stack frame
 	
-	uint8_t Loop;
-	uint16_t temp;
+	/*
+	 * Store SP value to temp variable
+	 */
+	register uint16_t temp asm("R7");
+	temp = __get_SP_register();
 
-	Task[TaskId].Id=TaskId;
-	Task[TaskId].Address = (uint16_t)pFun;
-	Task[TaskId].TCBLoc= (uint16_t)(TCB_BASE-(TASK_SIZE*TaskId));
-	
-	asm("mov r1,%[dst]":[dst]"=r"(temp)::"r1");
-	asm("mov %[src],r1"::[src]"r"(Task[TaskId].TCBLoc):"r1");
-	asm("push %[src]"::[src]"r"(Task[TaskId].Address));
-	asm("push #0x0008");
+	/*
+	 * Store argument in other registers because compiler pushes original
+	 * argument registers R12 and R13 into stack after entering into function.
+	 * we are manipulating stack for initialization so stack alignment can be lost
+	 * and original R12 and R13 values will be inaccessible.
+	 */
+	register uint16_t id asm("R8") = (uint16_t)taskId;
+	register uint16_t add asm("R9") = (uint16_t) pFun;
+	register uint16_t loop asm("R10");
 
-	
+	/*
+	 * Initialize task structure
+	 */
+	Task[taskId].id=id;
+	Task[taskId].address = add;
+	Task[taskId].TCBLoc= (uint16_t)(TCB_BASE-(TASK_SIZE*id));
+	Task[taskId].prevStack = 0x3F6;
+
+	/*
+	 * Change Stack pointer to task TCB
+	 */
+	asm volatile("mov %[src],R1"::[src]"r"(Task[id].TCBLoc):"R1");
+
+
+	/*
+	 * Push program Counter,Status register and more 12 registers
+	 */
+	asm volatile("push %[src]"::[src]"r"(add):"R1");
+	asm volatile("push #0x0008":::"R1");
 	//initialize task stack frame with zero
-	for(Loop=0;Loop<12;Loop++){
+	for(loop=0;loop<12;loop++){
 	
 		asm("push #0x0000");
 
 	}
 	
-	asm("mov r1,%[dst]":[dst]"=r"(Task[TaskId].PrevStack)::"r1"); //load sp
-	asm("mov %[src],r1"::[src]"r"(temp):"r1");
-	
+	/*
+	 * task TCB current stack pointer value is saved
+	 */
+	asm volatile("mov R1,%[dst]":[dst]"=r"(Task[id].prevStack)::); //load sp
+
+	/*
+	 * Back to previous stack value
+	 */
+	__set_SP_register(temp);
 }
 
-void RunTask(uint8_t TaskId){ //run a task
+void RunTask(uint8_t taskId){ //run a task
 
 
 	
-	CurrentTask=TaskId; //set running task
-	SchedulerInit();
-        asm("mov %[src],r1"::[src]"r"(Task[TaskId].PrevStack)); //load sp
-	RESTORE_CONTEXT(); //pop out task
+	CurrentTask=taskId; //set running task
+
+	SchedulerInit(); //Initialize timer
+
+    asm("mov %[src],r1"::[src]"m"(Task[taskId].prevStack)); //load sp
+
+    RESTORE_CONTEXT(); //pop out task
+
 }
 
--- kernel.h
+++ kernel.h
@@ -1,8 +1,20 @@
 #ifndef KERNEL_H_
 #define KERNEL_H_
 
+#include <stdint.h>
+
 void SchedulerInit(void);
-void TaskInit(void (*pFun)(void),uint8_t TaskId);
+void TaskInit(void (*pFun)(void),uint16_t TaskId);
 void RunTask(uint8_t TaskId);
+
+
+struct sTask{//Task Structure
+
+	uint16_t id;		//Task id
+	uint16_t address;	//Task Address
+	uint16_t prevStack;	//Task previous SP position
+	uint16_t TCBLoc;	//TCB location
+
+};
 
 #endif
